0d2af8d1816a8dc6a40d83b61f94814d3ded6c52
Jubeen Shah <jnshah2@ncsu.edu>
2018-10-31 07:54:46 -0400

    update to init_bsm in bsm.c
    update to init_bsm in bsm.c

--- csc501-lab2/paging/bsm.c
+++ csc501-lab2/paging/bsm.c
@@ -5,16 +5,42 @@
 #include <paging.h>
 #include <proc.h>
 
+#define SETZERO 0
+#define SETONE  1
+#define TWOTEN  1024
 /*-------------------------------------------------------------------------
  * init_bsm- initialize bsm_tab
  *-------------------------------------------------------------------------
  */
-SYSCALL init_bsm()
-{
+SYSCALL init_bsm(){
+
+  STATWORD ps;
+  disable(ps);
+
+  int index = SETZERO;
+  int indexDos;
+  int twoFourTen = TWOTEN * 4;
+  while (index<8) { //8 = Number of backing Store
+    /* code */
+    bsm_tab[index].bs_status = SETZERO;
+    indexDos = SETZERO;
+    while (indexDos < NPROC) {
+      /* code */
+      bsm_tab[index].bs_pid[indexDos] = SETZERO;
+      bsm_tab[index].bs_vpno[indexDos] = twoFourTen;
+      indexDos = indexDos + SETONE;
+    }
+    bsm_tab[index].bs_npages  = SETZERO;
+    bsm_tab[index].bs_sem     = SETZERO;
+    bsm_tab[index].bs_private = SETZERO;
+    bsm_tab[index].bs_mapping = SETZERO;
+
+    index = index + SETONE;
+  }
 }
 
 /*-------------------------------------------------------------------------
- * get_bsm - get a free entry from bsm_tab 
+ * get_bsm - get a free entry from bsm_tab
  *-------------------------------------------------------------------------
  */
 SYSCALL get_bsm(int* avail)
@@ -23,7 +49,7 @@ SYSCALL get_bsm(int* avail)
 
 
 /*-------------------------------------------------------------------------
- * free_bsm - free an entry from bsm_tab 
+ * free_bsm - free an entry from bsm_tab
  *-------------------------------------------------------------------------
  */
 SYSCALL free_bsm(int i)
@@ -40,7 +66,7 @@ SYSCALL bsm_lookup(int pid, long vaddr, int* store, int* pageth)
 
 
 /*-------------------------------------------------------------------------
- * bsm_map - add an mapping into bsm_tab 
+ * bsm_map - add an mapping into bsm_tab
  *-------------------------------------------------------------------------
  */
 SYSCALL bsm_map(int pid, int vpno, int source, int npages)
@@ -56,5 +82,3 @@ SYSCALL bsm_map(int pid, int vpno, int source, int npages)
 SYSCALL bsm_unmap(int pid, int vpno, int flag)
 {
 }
-
-
