d77416930e20c97142fabfa566850c759032537a
Jubeen Shah <jnshah2@ncsu.edu>
2018-10-31 18:18:55 -0400

    Update csc501-lab2/paging/xm.c

--- csc501-lab2/paging/xm.c
+++ csc501-lab2/paging/xm.c
@@ -5,6 +5,9 @@
 #include <proc.h>
 #include <paging.h>
 
+#define SETZERO 0
+#define SETONE  1
+#define TWOTEN  1024
 
 /*-------------------------------------------------------------------------
  * xmmap - xmmap
@@ -12,8 +15,39 @@
  */
 SYSCALL xmmap(int virtpage, bsd_t source, int npages)
 {
-  kprintf("xmmap - to be implemented!\n");
-  return SYSERR;
+  //kprintf("xmmap - to be implemented!\n");
+  STATWORD ps;
+  disable(ps);
+
+  int c = currpid;
+  int v = virtpage;
+  int s = source;
+  int n = npages;
+  if (virtpage < (4*TWOTEN) || source < SETZERO || source >= (TWOTEN/128) || npages < SETONE || npages > (TWOTEN / 4)) {
+    /* code */
+    restore(ps);
+    return -SETONE;
+  }
+
+  int checkPrivateValue = bsm_tab[source].bs_private;
+
+  if (checkPrivateValue == SETONE) {
+    /* code */
+    restore(ps);
+    return -SETONE;
+  }
+  int checkMapVal = bsm_tab[source].bs_mapping;
+  int checkNpages = bsm_tab[source].bs_npages;
+  if (checkMapVal > SETZERO && npages > checkNpages) {
+    /* code */
+    restore(ps);
+    return -SETONE;
+  }
+
+  bsm_map(c,v,s,n);
+
+  restore(ps);
+  return SETONE;
 }
 
 
@@ -24,6 +58,17 @@ SYSCALL xmmap(int virtpage, bsd_t source, int npages)
  */
 SYSCALL xmunmap(int virtpage)
 {
-  kprintf("To be implemented!");
-  return SYSERR;
+  //kprintf("To be implemented!");
+
+  STATWORD ps;
+  disable(ps);
+
+  if (virtpage < (TWOTEN * 4)) {
+    restore(ps);
+    return -SETONE;
+  }
+
+  bsm_unmap(currpid, virtpage);
+  restore(ps);
+  return SETONE;
 }
