f224ee6558226869a0ff4a4b9a4dd05442bdfee4
Jubeen Shah <jnshah2@ncsu.edu>
2018-10-31 13:58:50 -0400

    Update csc501-lab2/paging/release_bs.c

--- csc501-lab2/paging/release_bs.c
+++ csc501-lab2/paging/release_bs.c
@@ -3,11 +3,30 @@
 #include <proc.h>
 #include <paging.h>
 
+#define SETONE  1
+#define SETZERO 0
+#define TWOTEN  1024
+
 SYSCALL release_bs(bsd_t bs_id) {
 
   /* release the backing store with ID bs_id */
-    kprintf("To be implemented!\n");
+    //kprintf("To be implemented!\n");
+    STATWORD ps;
+    disable(ps);
+    int index = bs_id;
+    bsm_tab[index].bs_pid[currpid]  = SETZERO;
+    bsm_tab[index].bs_vpno[currpid] = TWOTEN * 4;
+
+    int checkMapping = bsm_tab[index].bs_mapping;
+    if (checkMapping == SETZERO) {
+      /* code */
+      bsm_tab[index].bs_status = SETZERO;
+      bsm_tab[index].bs_sem    = SETZERO;
+      bsm_tab[index].bs_npages = SETZERO;
+      bsm_tab[index].bs_private= SETZERO;
+    }
+
+   restore(ps);
    return OK;
 
 }
-
