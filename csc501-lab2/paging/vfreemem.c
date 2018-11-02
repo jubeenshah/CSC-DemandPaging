bc76ef943932ee3b2644bf8ff5c9339083c48823
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:19:40 -0400

    Update csc501-lab2/paging/vfreemem.c

--- csc501-lab2/paging/vfreemem.c
+++ csc501-lab2/paging/vfreemem.c
@@ -1,91 +1,13 @@
-// /* vfreemem.c - vfreemem */
-//
-// #include <conf.h>
-// #include <kernel.h>
-// #include <mem.h>
-// #include <proc.h>
-//
-// #define SETZERO 0
-// #define SETONE  1
-// #define TWOTEN  1024
-//
-// extern struct pentry proctab[];
-// /*------------------------------------------------------------------------
-//  *  vfreemem  --  free a virtual memory block, returning it to vmemlist
-//  *------------------------------------------------------------------------
-//  */
-// SYSCALL	vfreemem(block, size)
-// 	struct	mblock	*block;
-// 	unsigned size;
-// {
-// 	kprintf("VFREEMEM To be implemented!\n");
-// 	STATWORD ps;
-// 	struct mblock *index;
-// 	unsigned top;
-// 	struct mblock *indexDos;
-// 	if (size == SETZERO) {
-// 		/* code */
-// 		return -SETONE;
-// 	}
-//
-// 	size = (unsigned) roundmb(size);
-// 	disable(ps);
-//
-// 	int list = &proctab[currpid].vmemlist;
-// 	indexDos = list;
-//
-// 	index = indexDos->mnext;
-//
-// 	while (index != (struct mblock *)NULL && index< block) {
-// 		indexDos = index;
-// 		index = index->mnext;
-// 	}
-//
-//
-// 		int c = indexDos->mlen;
-// 		int h = c + (unsigned)indexDos;
-// 		top = h;
-// 		int e = &proctab[currpid].vmemlist;
-// 		int at = size + (unsigned)block;
-// 		if ((top > (unsigned)block && indexDos != e) || (index != NULL && (at > (unsigned)index ) )) {
-// 			/* code */
-// 			restore(ps);
-// 			return -SETONE;
-// 		}
-// 		int ed = (unsigned) block;
-//
-// 		if (indexDos != &memlist && top == ed) {
-// 			/* code */
-// 			int add = indexDos->mlen;
-// 			indexDos->mlen =  add + size;
-// 		} else {
-// 			int s = size;
-// 			block->mlen = s;
-// 			block->mnext = index;
-// 			indexDos->mnext = block;
-// 			indexDos = block;
-// 		}
-// 		int a00 = indexDos->mlen;
-// 		int a01 = (unsigned)(indexDos);
-// 		int a02 = a00 + a01;
-// 		int b00 = (unsigned)index;
-// 		if (a02 == b00) {
-// 			/* code */
-// 			int addDos = indexDos->mlen;
-// 			indexDos->mlen = addDos + index->mlen;
-// 			indexDos->mnext = index->mnext;
-// 		}
-//
-// 	restore(ps);
-// 	return OK;
-// }
-
 /* vfreemem.c - vfreemem */
 
 #include <conf.h>
 #include <kernel.h>
 #include <mem.h>
 #include <proc.h>
+
+#define SETZERO 0
+#define SETONE  1
+#define TWOTEN  1024
 
 extern struct pentry proctab[];
 /*------------------------------------------------------------------------
@@ -96,39 +18,117 @@ SYSCALL	vfreemem(block, size)
 	struct	mblock	*block;
 	unsigned size;
 {
-	//kprintf("To be implemented!\n");
+	kprintf("VFREEMEM To be implemented!\n");
 	STATWORD ps;
-	struct mblock *p,*q;
+	struct mblock *index;
 	unsigned top;
-	if(size==0){
-		return SYSERR;
+	struct mblock *indexDos;
+	if (size == SETZERO) {
+		/* code */
+		return -SETONE;
 	}
-	size=(unsigned) roundmb(size);
+
+	size = (unsigned) roundmb(size);
 	disable(ps);
 
-	q=&proctab[currpid].vmemlist;
-	for(p=q->mnext;p!=(struct mblock*)NULL && p<block;q=p,p=p->mnext);
+	int list = &proctab[currpid].vmemlist;
+	indexDos = list;
 
-	if(((top=q->mlen+ (unsigned)q)>(unsigned)block && q!=&proctab[currpid].vmemlist) || (p!=NULL && (size+(unsigned)block)> (unsigned)p)  ){
-		restore(ps);
-		return SYSERR;
+	index = indexDos->mnext;
+
+	while (index != (struct mblock *)NULL && index< block) {
+		indexDos = index;
+		index = index->mnext;
 	}
 
-	if(q!=&memlist && top==(unsigned) block){
-		q->mlen+=size;
-	}
-	else{
-		block->mlen=size;
-		block->mnext=p;
-		q->mnext=block;
-		q=block;
-	}
-	if((unsigned)(q->mlen + (unsigned)q)==(unsigned)p){
-		q->mlen+=p->mlen;
-		q->mnext=p->mnext;
-	}
+
+		int c = indexDos->mlen;
+		int h = c + (unsigned)indexDos;
+		top = h;
+		int e = &proctab[currpid].vmemlist;
+		int at = size + (unsigned)block;
+		if ((top > (unsigned)block && indexDos != e) || (index != NULL && (at > (unsigned)index ) )) {
+			/* code */
+			restore(ps);
+			return -SETONE;
+		}
+		int ed = (unsigned) block;
+
+		if (indexDos != &memlist && top == ed) {
+			/* code */
+			int add = indexDos->mlen;
+			indexDos->mlen =  add + size;
+		} else {
+			int s = size;
+			block->mlen = s;
+			block->mnext = index;
+			indexDos->mnext = block;
+			indexDos = block;
+		}
+		int a00 = indexDos->mlen;
+		int a01 = (unsigned)(indexDos);
+		int a02 = a00 + a01;
+		int b00 = (unsigned)index;
+		if (a02 == b00) {
+			/* code */
+			int addDos = indexDos->mlen;
+			indexDos->mlen = addDos + index->mlen;
+			indexDos->mnext = index->mnext;
+		}
 
 	restore(ps);
-	return(OK);
-
+	return OK;
 }
+
+// /* vfreemem.c - vfreemem */
+//
+// #include <conf.h>
+// #include <kernel.h>
+// #include <mem.h>
+// #include <proc.h>
+//
+// extern struct pentry proctab[];
+// /*------------------------------------------------------------------------
+//  *  vfreemem  --  free a virtual memory block, returning it to vmemlist
+//  *------------------------------------------------------------------------
+//  */
+// SYSCALL	vfreemem(block, size)
+// 	struct	mblock	*block;
+// 	unsigned size;
+// {
+// 	//kprintf("To be implemented!\n");
+// 	STATWORD ps;
+// 	struct mblock *p,*q;
+// 	unsigned top;
+// 	if(size==0){
+// 		return SYSERR;
+// 	}
+// 	size=(unsigned) roundmb(size);
+// 	disable(ps);
+//
+// 	q=&proctab[currpid].vmemlist;
+// 	for(p=q->mnext;p!=(struct mblock*)NULL && p<block;q=p,p=p->mnext);
+//
+// 	if(((top=q->mlen+ (unsigned)q)>(unsigned)block && q!=&proctab[currpid].vmemlist) || (p!=NULL && (size+(unsigned)block)> (unsigned)p)  ){
+// 		restore(ps);
+// 		return SYSERR;
+// 	}
+//
+// 	if(q!=&memlist && top==(unsigned) block){
+// 		q->mlen+=size;
+// 	}
+// 	else{
+// 		block->mlen=size;
+// 		block->mnext=p;
+// 		q->mnext=block;
+// 		q=block;
+// 	}
+// 	if((unsigned)(q->mlen + (unsigned)q)==(unsigned)p){
+// 		q->mlen+=p->mlen;
+// 		q->mnext=p->mnext;
+// 	}
+//
+// 	restore(ps);
+// 	return(OK);
+//
+// }
