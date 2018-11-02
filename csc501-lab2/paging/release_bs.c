6b3ba6b7d104a598949dba980856abf824b6c8ad
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:24:23 -0400

    Update csc501-lab2/paging/release_bs.c

--- csc501-lab2/paging/release_bs.c
+++ csc501-lab2/paging/release_bs.c
@@ -1,62 +1,62 @@
-// #include <conf.h>
-// #include <kernel.h>
-// #include <proc.h>
-// #include <paging.h>
-//
-// #define SETONE  1
-// #define SETZERO 0
-// #define TWOTEN  1024
-// #define TWOTWE  4096
-//
-// SYSCALL release_bs(bsd_t bs_id) {
-//
-//   /* release the backing store with ID bs_id */
-//     kprintf("RELEASE_BSTo be implemented!\n");
-//     STATWORD ps;
-//     disable(ps);
-//     int index = bs_id;
-//     bsm_tab[index].bs_pid[currpid]  = SETZERO;
-//     bsm_tab[index].bs_vpno[currpid] = TWOTWE;
-//
-//     int checkMapping = bsm_tab[index].bs_mapping;
-//     if (checkMapping == SETZERO) {
-//       /* code */
-//       bsm_tab[index].bs_status = SETZERO;
-//       bsm_tab[index].bs_sem    = SETZERO;
-//       bsm_tab[index].bs_npages = SETZERO;
-//       bsm_tab[index].bs_private= SETZERO;
-//     }
-//
-//    restore(ps);
-//    return OK;
-//
-// }
-
 #include <conf.h>
 #include <kernel.h>
 #include <proc.h>
 #include <paging.h>
 
+#define SETONE  1
+#define SETZERO 0
+#define TWOTEN  1024
+#define TWOTWE  4096
+
 SYSCALL release_bs(bsd_t bs_id) {
 
   /* release the backing store with ID bs_id */
-  //  kprintf("To be implemented!\n");
-  STATWORD ps;
-  disable(ps);
-  bsm_tab[bs_id].bs_pid[currpid]=0;
-  bsm_tab[bs_id].bs_vpno[currpid]=4096;
+    kprintf("RELEASE_BSTo be implemented!\n");
+    STATWORD ps;
+    disable(ps);
+    int index = bs_id;
+    bsm_tab[index].bs_pid[currpid]  = SETZERO;
+    bsm_tab[index].bs_vpno[currpid] = TWOTWE;
 
-if(bsm_tab[bs_id].bs_mapn==0){
-  bsm_tab[bs_id].bs_status=BSM_UNMAPPED;
-  bsm_tab[bs_id].bs_sem=0;
-  //bsm_tab[bs_id].bs_pid=-1;
-  bsm_tab[bs_id].bs_npages=0;
-  //bsm_tab[bs_id].bs_vpno=4096;
-  bsm_tab[bs_id].bs_private=0;
+    int checkMapping = bsm_tab[index].bs_mapn;
+    if (checkMapping == SETZERO) {
+      /* code */
+      bsm_tab[index].bs_status = SETZERO;
+      bsm_tab[index].bs_sem    = SETZERO;
+      bsm_tab[index].bs_npages = SETZERO;
+      bsm_tab[index].bs_private= SETZERO;
+    }
 
-  //kprintf("FINALLY release store%d\n",bs_id);
+   restore(ps);
+   return OK;
+
 }
-//	kprintf("release_bs pid:%d mapn:%d\n",currpid,bsm_tab[bs_id].bs_mapn);
-	restore(ps);
-	return OK;
-}
+
+// #include <conf.h>
+// #include <kernel.h>
+// #include <proc.h>
+// #include <paging.h>
+//
+// SYSCALL release_bs(bsd_t bs_id) {
+//
+//   /* release the backing store with ID bs_id */
+//   //  kprintf("To be implemented!\n");
+//   STATWORD ps;
+//   disable(ps);
+//   bsm_tab[bs_id].bs_pid[currpid]=0;
+//   bsm_tab[bs_id].bs_vpno[currpid]=4096;
+//
+// if(bsm_tab[bs_id].bs_mapn==0){
+//   bsm_tab[bs_id].bs_status=BSM_UNMAPPED;
+//   bsm_tab[bs_id].bs_sem=0;
+//   //bsm_tab[bs_id].bs_pid=-1;
+//   bsm_tab[bs_id].bs_npages=0;
+//   //bsm_tab[bs_id].bs_vpno=4096;
+//   bsm_tab[bs_id].bs_private=0;
+//
+//   //kprintf("FINALLY release store%d\n",bs_id);
+// }
+// //	kprintf("release_bs pid:%d mapn:%d\n",currpid,bsm_tab[bs_id].bs_mapn);
+// 	restore(ps);
+// 	return OK;
+// }
