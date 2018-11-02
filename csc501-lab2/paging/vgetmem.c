df0b5e5bcc6cbffb078b99e77882659e35f12961
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 10:16:41 -0400

    test

--- csc501-lab2/paging/vgetmem.c
+++ csc501-lab2/paging/vgetmem.c
@@ -6,6 +6,10 @@
 #include <proc.h>
 #include <paging.h>
 
+#define SETZERO 0
+#define SETONE  1
+#define TWOTEN  1024
+
 extern struct pentry proctab[];
 /*------------------------------------------------------------------------
  * vgetmem  --  allocate virtual heap storage, returning lowest WORD address
@@ -14,34 +18,49 @@ extern struct pentry proctab[];
 WORD	*vgetmem(nbytes)
 	unsigned nbytes;
 {
-
-	//kprintf("To be implemented!\n");
 	STATWORD ps;
-	struct mblock *p, *q, *leftover;
-	disable(ps);
+	struct mblock *index;
+	struct mblock *indexDos;
+	struct mblock *indexTres;
+	kprintf("VGETMEM To be implemented!\n");
 
-	if(nbytes==0 || proctab[currpid].vmemlist->mnext== (struct mblock*) NULL ){
-		restore(ps);
-		return SYSERR;
-	}
-	nbytes=(unsigned int ) roundmb(nbytes);
-	q=&proctab[currpid].vmemlist;
-	for(p=q->mnext;p!=(struct mblock*) NULL ;q=p,p=p->mnext){
-		if(p->mlen==nbytes){
-			q->mnext=p->mnext;
-			restore(ps);
-			return ((WORD*)p);
-		}
-		else if(p->mlen>nbytes){
-			leftover=(struct mblock *)((unsigned)p + nbytes);
-			q->mnext=leftover;
-			leftover->mnext=p->mnext;
-			leftover->mlen=p->mlen-nbytes;
-			restore(ps);
-			return ((WORD*)p);
-		}
-	}
+int checkList = proctab[currpid].vmemlist->mnext;
+
+if (nbytes == SETZERO || checkList == (struct mblock*)NULL) {
+	/* code */
 	restore(ps);
+	return -SETONE;
+}
 
-	return((WORD*) SYSERR );
+int listFor;
+nbytes = (unsigned int) roundmb(nbytes);
+listFor = &proctab[currpid].vmemlist;
+indexDos = listFor;
+index = indexDos->mnext;
+while (index != (struct mblock *)NULL) {
+	/* code */
+	int checkMlen = index->mlen;
+	if (checkMlen == nbytes) {
+		/* code */
+		indexDos->mnext = index->mnext;
+		restore (ps);
+		return ((WORD*)index);
+	} else if(checkMlen > nbytes) {
+		int conv = (unsigned)index + nbytes;
+		indexTres = (struct mblock *) (conv);
+
+		indexDos->mnext = indexTres;
+		indexTres->mnext = index->mnext;
+		int sub = index->mlen - nbytes;
+		indexTres->mlen = sub;
+
+		restore(ps);
+		return ((WORD*)index);
+	}
+
+	indexDos = index;
+	index = index->mnext;
+}
+	restore(ps);
+	return((WORD*) -SETONE );
 }
--- csc501-lab2/paging/xm.c
+++ csc501-lab2/paging/xm.c
@@ -5,9 +5,6 @@
 #include <proc.h>
 #include <paging.h>
 
-#define SETZERO 0
-#define SETONE  1
-#define TWOTEN  1024
 
 /*-------------------------------------------------------------------------
  * xmmap - xmmap
@@ -15,37 +12,24 @@
  */
 SYSCALL xmmap(int virtpage, bsd_t source, int npages)
 {
-  kprintf("xmmap - to be implemented!\n");
+ // kprintf("xmmap - to be implemented!\n");
   STATWORD ps;
   disable(ps);
 
-
-  if (virtpage < (4*TWOTEN) || source < SETZERO || source >= (TWOTEN/128) || npages < SETONE || npages > (TWOTEN / 4)) {
-    /* code */
-    restore(ps);
-    return SYSERR;
+  if(virtpage<4096 || source<0 || source>=NBS || npages<1 || npages>NPPBS){
+  	restore(ps);
+	return SYSERR;
+  }
+  if(bsm_tab[source].bs_private==1){
+  	restore(ps);
+	return SYSERR;
+  }
+  if(bsm_tab[source].bs_mapn>0 && npages> bsm_tab[source].bs_npages){
+  	restore(ps);
+	return SYSERR;
   }
 
-  int checkPrivateValue = bsm_tab[source].bs_private;
-
-  if (checkPrivateValue == SETONE) {
-    /* code */
-    restore(ps);
-    return SYSERR;
-  }
-  int checkMapVal = bsm_tab[source].bs_mapping;
-  int checkNpages = bsm_tab[source].bs_npages;
-  if (checkMapVal > SETZERO && npages > checkNpages) {
-    /* code */
-    restore(ps);
-    return SYSERR;
-  }
-  int c = currpid;
-  int v = virtpage;
-  int s = source;
-  int n = npages;
-  bsm_map(c,v,s,n);
-  //bsm_map(currpid,virtpage,source,npages);
+  bsm_map(currpid,virtpage,source,npages);
 
   restore(ps);
   return OK;
@@ -60,16 +44,15 @@ SYSCALL xmmap(int virtpage, bsd_t source, int npages)
 SYSCALL xmunmap(int virtpage)
 {
   //kprintf("To be implemented!");
-
   STATWORD ps;
   disable(ps);
 
-  if (virtpage < (TWOTEN * 4)) {
-    restore(ps);
-    return SYSERR;
+  if(virtpage<4096){
+  	restore(ps);
+	return SYSERR;
   }
 
-  bsm_unmap(currpid, virtpage);
+  bsm_unmap(currpid,virtpage);
   restore(ps);
   return OK;
 }
