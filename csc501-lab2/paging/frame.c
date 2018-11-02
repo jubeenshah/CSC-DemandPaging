018f728a2343b04c01cee698b595c79bf594f4cd
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 10:52:00 -0400

    Update csc501-lab2/paging/frame.c

--- csc501-lab2/paging/frame.c
+++ csc501-lab2/paging/frame.c
@@ -255,7 +255,7 @@ SYSCALL init_frm()
 		frm_tab[i].fr_dirty=0;
 
 		lfu_cnt[i]=0; /* LFU counter*/
-		sc_acc[i]=0;/* SC policy */
+		scA[i]=0;/* SC policy */
 		scPointer=0;
 	}
 	restore(ps);
@@ -279,7 +279,7 @@ SYSCALL get_frm(int* avail)
 			*avail=i;
 
 			lfu_cnt[i]++;
-			sc_acc[i]=1;
+			scA[i]=1;
 			restore(ps);
 			return OK;
 		}
@@ -288,7 +288,7 @@ SYSCALL get_frm(int* avail)
 	if(page_replace_policy== SC){
 		frame_number=get_frm_SC();
 		free_frm(frame_number);
-		sc_acc[frame_number]=1;
+		scA[frame_number]=1;
 		*avail=frame_number;
 		restore(ps);
 		return OK;
@@ -397,8 +397,8 @@ int get_frm_SC(){
 	while(1){
 		i=i%NFRAMES;
 		if(frm_tab[i].fr_type==FR_PAGE){
-			if(sc_acc[i]==1){
-				sc_acc[i]=0;
+			if(scA[i]==1){
+				scA[i]=0;
 			}
 			else{
 				scPointer=i+1;
