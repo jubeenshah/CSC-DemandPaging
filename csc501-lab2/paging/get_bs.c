b70973820e413a3d074d4f8fb12c8b0f0e3188fe
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:01:48 -0400

    Update csc501-lab2/paging/get_bs.c

--- csc501-lab2/paging/get_bs.c
+++ csc501-lab2/paging/get_bs.c
@@ -1,81 +1,81 @@
-// #include <conf.h>
-// #include <kernel.h>
-// #include <proc.h>
-// #include <paging.h>
-//
-// #define SETONE  1
-// #define SETZERO 0
-// #define NEIGHT  16 //8
-// #define NPAGES  128 //256
-// int get_bs(bsd_t bs_id, unsigned int npages) {
-//
-// STATWORD ps;
-// disable(ps);
-//   /* requests a new mapping of npages with ID map_id */
-//     kprintf("get_bs\n");
-//     int checkSemValue     = bsm_tab[bs_id].bs_sem;
-//     int checkPrivateValue = bsm_tab[bs_id].bs_private;
-//     int checkStatus       = bsm_tab[bs_id].bs_status;
-//     int pagesValue        = bsm_tab[bs_id].bs_npages;
-//     if (bs_id >= NEIGHT || bs_id < SETZERO || npages > NPAGES ||
-//           npages <= SETZERO || checkSemValue == SETONE) {
-//       /* code */
-//       restore(ps);
-//       return SYSERR;
-//     }
-//
-//     if (checkPrivateValue == SETONE && checkStatus == SETONE) {
-//       /* code */
-//       restore(ps);
-//       return SYSERR;
-//     }
-//
-//     if (checkPrivateValue == SETZERO && checkStatus == SETONE) {
-//       /* code */
-//       restore(ps);
-//       pagesValue = bsm_tab[bs_id].bs_npages;
-//       return pagesValue;
-//     }
-//
-//     pagesValue = npages;
-//     bsm_tab[bs_id].bs_npages = pagesValue;
-//
-//     bsm_tab[bs_id].bs_pid[currpid] = SETONE;
-//
-//
-//     restore(ps);
-//     return npages;
-//
-// }
-
 #include <conf.h>
 #include <kernel.h>
 #include <proc.h>
 #include <paging.h>
 
+#define SETONE  1
+#define SETZERO 0
+#define NEIGHT  8 //8
+#define NPAGES  256 //256
 int get_bs(bsd_t bs_id, unsigned int npages) {
 
+STATWORD ps;
+disable(ps);
   /* requests a new mapping of npages with ID map_id */
-  //  kprintf("To be implemented!\n");
-  STATWORD ps;
-  disable(ps);
+    kprintf("get_bs\n");
+    int checkSemValue     = bsm_tab[bs_id].bs_sem;
+    int checkPrivateValue = bsm_tab[bs_id].bs_private;
+    int checkStatus       = bsm_tab[bs_id].bs_status;
+    int pagesValue        = bsm_tab[bs_id].bs_npages;
+    if (bs_id >= NEIGHT || bs_id < SETZERO || npages > NPAGES ||
+          npages <= SETZERO || checkSemValue == SETONE) {
+      /* code */
+      restore(ps);
+      return SYSERR;
+    }
 
-  if(bs_id>=NBS || bs_id<0 || npages>NPPBS || npages<=0 || bsm_tab[bs_id].bs_sem==1){
-  	restore(ps);
-	return SYSERR;
-  }
-  if(bsm_tab[bs_id].bs_private==1 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
-  	restore(ps);
-	return SYSERR;
-  }
-  if(bsm_tab[bs_id].bs_private==0 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
-  	restore(ps);
-	return bsm_tab[bs_id].bs_npages;
-  }
-  /* bsm_tab[bs_id].bs_status == BSM_UNMAPPED */
-	bsm_tab[bs_id].bs_npages=npages;
-	bsm_tab[bs_id].bs_pid[currpid]=1;
+    if (checkPrivateValue == SETONE && checkStatus == SETONE) {
+      /* code */
+      restore(ps);
+      return SYSERR;
+    }
 
-	restore(ps);
+    if (checkPrivateValue == SETZERO && checkStatus == SETONE) {
+      /* code */
+      restore(ps);
+      pagesValue = bsm_tab[bs_id].bs_npages;
+      return pagesValue;
+    }
+
+    pagesValue = npages;
+    bsm_tab[bs_id].bs_npages = pagesValue;
+
+    bsm_tab[bs_id].bs_pid[currpid] = SETONE;
+
+
+    restore(ps);
     return npages;
+
 }
+
+// #include <conf.h>
+// #include <kernel.h>
+// #include <proc.h>
+// #include <paging.h>
+//
+// int get_bs(bsd_t bs_id, unsigned int npages) {
+//
+//   /* requests a new mapping of npages with ID map_id */
+//   //  kprintf("To be implemented!\n");
+//   STATWORD ps;
+//   disable(ps);
+//
+//   if(bs_id>=NBS || bs_id<0 || npages>NPPBS || npages<=0 || bsm_tab[bs_id].bs_sem==1){
+//   	restore(ps);
+// 	return SYSERR;
+//   }
+//   if(bsm_tab[bs_id].bs_private==1 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
+//   	restore(ps);
+// 	return SYSERR;
+//   }
+//   if(bsm_tab[bs_id].bs_private==0 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
+//   	restore(ps);
+// 	return bsm_tab[bs_id].bs_npages;
+//   }
+//   /* bsm_tab[bs_id].bs_status == BSM_UNMAPPED */
+// 	bsm_tab[bs_id].bs_npages=npages;
+// 	bsm_tab[bs_id].bs_pid[currpid]=1;
+//
+// 	restore(ps);
+//     return npages;
+// }
