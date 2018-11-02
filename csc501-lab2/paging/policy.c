eb3797bcca70d3409be5c7eb255aff7cc863b8da
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:08:47 -0400

    Update csc501-lab2/paging/policy.c

--- csc501-lab2/paging/policy.c
+++ csc501-lab2/paging/policy.c
@@ -1,33 +1,3 @@
-// /* policy.c = srpolicy*/
-//
-// #include <conf.h>
-// #include <kernel.h>
-// #include <paging.h>
-//
-//
-// extern int page_replace_policy;
-// /*-------------------------------------------------------------------------
-//  * srpolicy - set page replace policy
-//  *-------------------------------------------------------------------------
-//  */
-// SYSCALL srpolicy(int policy)
-// {
-//   /* sanity check ! */
-//
-//   kprintf("POLICITY To be implemented!\n");
-//   int setPolicy = policy;
-//   page_replace_policy = setPolicy;
-//   return OK;
-// }
-//
-// /*-------------------------------------------------------------------------
-//  * grpolicy - get page replace policy
-//  *-------------------------------------------------------------------------
-//  */
-// SYSCALL grpolicy()
-// {
-//   return page_replace_policy;
-// }
 /* policy.c = srpolicy*/
 
 #include <conf.h>
@@ -44,8 +14,9 @@ SYSCALL srpolicy(int policy)
 {
   /* sanity check ! */
 
-  //kprintf("To be implemented!\n");
-	page_replace_policy=policy;
+  kprintf("POLICITY To be implemented!\n");
+  int setPolicy = policy;
+  page_replace_policy = setPolicy;
   return OK;
 }
 
@@ -57,3 +28,32 @@ SYSCALL grpolicy()
 {
   return page_replace_policy;
 }
+// /* policy.c = srpolicy*/
+//
+// #include <conf.h>
+// #include <kernel.h>
+// #include <paging.h>
+//
+//
+// extern int page_replace_policy;
+// /*-------------------------------------------------------------------------
+//  * srpolicy - set page replace policy
+//  *-------------------------------------------------------------------------
+//  */
+// SYSCALL srpolicy(int policy)
+// {
+//   /* sanity check ! */
+//
+//   //kprintf("To be implemented!\n");
+// 	page_replace_policy=policy;
+//   return OK;
+// }
+//
+// /*-------------------------------------------------------------------------
+//  * grpolicy - get page replace policy
+//  *-------------------------------------------------------------------------
+//  */
+// SYSCALL grpolicy()
+// {
+//   return page_replace_policy;
+// }
