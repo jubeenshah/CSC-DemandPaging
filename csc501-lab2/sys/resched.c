5e7acbef6708bfd9d5cf99adc7f8e6933a05916a
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:44:30 -0400

    Update csc501-lab2/sys/resched.c

--- csc501-lab2/sys/resched.c
+++ csc501-lab2/sys/resched.c
@@ -89,82 +89,82 @@ int	resched()
 
 	int oldProcessID = optr - proctab;
 	int old_pid = oldProcessID;
-	// int store;
-	// int pageth;
-	// int hasToLookup;
-	// int res_lookup = hasToLookup;
+	int store;
+	int pageth;
+	int hasToLookup;
+	int res_lookup = hasToLookup;
 
 	//int old_pid=optr-proctab;
-	int store,pageth,res_lookup;
+	// int store,pageth,res_lookup;
 
 
-	// int index = SETZERO;
-	// while (index < TWOTEN) {
-	// 	/* code */
-	// 	int checkPid = frm_tab[index].fr_pid;
-	// 	int checkTyp = frm_tab[index].fr_type;
-	// 	if (checkPid == oldProcessID && checkTyp == SETZERO) {
-	// 		/* code */
-	// 		// int q = oldProcessID;
-	// 		// int w = frm_tab[index].fr_vpno * TWOTEN * 4;
-	//
-	// 		hasToLookup = bsm_lookup(oldProcessID,(frm_tab[index].fr_vpno * TWOTEN * 4),&store, &pageth);
-	// 		if (hasToLookup == SYSERR) {
-	// 			continue;
-	// 		}
-	// 		// int e = index + TWOTEN;
-	// 		// e = e * TWOTEN * 4;
-	// 		write_bs(((index + TWOTEN)*TWOTEN*4), store, pageth);
-	// 	}
-	// 	index = index + SETONE;
-	// }
+	int index = SETZERO;
+	while (index < TWOTEN) {
+		/* code */
+		int checkPid = frm_tab[index].fr_pid;
+		int checkTyp = frm_tab[index].fr_type;
+		if (checkPid == oldProcessID && checkTyp == SETZERO) {
+			/* code */
+			// int q = oldProcessID;
+			// int w = frm_tab[index].fr_vpno * TWOTEN * 4;
 
-	for(i=0;i<NFRAMES;++i){
-	if(frm_tab[i].fr_pid==old_pid && frm_tab[i].fr_type==FR_PAGE ){
-		res_lookup=bsm_lookup(old_pid,frm_tab[i].fr_vpno*NBPG,&store,&pageth);
-		if(res_lookup==SYSERR){
-			continue;
-		}
-	//	kprintf("write_bs pid:%d vaddr:%08x store:%d pageth:%d\n",pid,vaddr,store,pageth);
-		write_bs((i+FRAME0)*NBPG,store,pageth);
-	}
-
-}
-
-
-
-
-// index = SETZERO;
-// 	while (index < TWOTEN) {
-// 		/* code */
-// 		int checkPIDNew = frm_tab[index].fr_pid;
-// 		int checkTypeNew = frm_tab[index].fr_type;
-//
-// 		if (checkPIDNew == currpid && checkTypeNew == SETZERO) {
-// 			/* code */
-// 			int vpnoTolookup = frm_tab[index].fr_vpno;
-// 			// vpnoTolookup = vpnoTolookup * TWOTEN * 4;
-// 			hasToLookup = bsm_lookup(currpid, (vpnoTolookup*TWOTEN * 4), &store, &pageth);
-// 			if (hasToLookup == SYSERR) {
-// 				continue;
-// 			}
-// 			int frameInt = (index + TWOTEN)* TWOTEN * 4;
-// 			// frameInt = frameInt * TWOTEN * 4;
-// 			read_bs(frameInt, store, pageth);
-// 		}
-// 		index = index + SETONE;
-// 	}
-
-	for(i=0;i<NFRAMES;++i){
-		 if(frm_tab[i].fr_pid==currpid && frm_tab[i].fr_type==FR_PAGE){
-			res_lookup=bsm_lookup(currpid,frm_tab[i].fr_vpno*NBPG,&store,&pageth);
-			if(res_lookup==SYSERR){
+			hasToLookup = bsm_lookup(oldProcessID,(frm_tab[index].fr_vpno * TWOTEN * 4),&store, &pageth);
+			if (hasToLookup == SYSERR) {
 				continue;
 			}
-			read_bs((i+FRAME0)*NBPG,store,pageth);
+			// int e = index + TWOTEN;
+			// e = e * TWOTEN * 4;
+			write_bs(((index + TWOTEN)*TWOTEN*4), store, pageth);
 		}
-
+		index = index + SETONE;
 	}
+
+// 	for(i=0;i<NFRAMES;++i){
+// 	if(frm_tab[i].fr_pid==old_pid && frm_tab[i].fr_type==FR_PAGE ){
+// 		res_lookup=bsm_lookup(old_pid,frm_tab[i].fr_vpno*NBPG,&store,&pageth);
+// 		if(res_lookup==SYSERR){
+// 			continue;
+// 		}
+// 	//	kprintf("write_bs pid:%d vaddr:%08x store:%d pageth:%d\n",pid,vaddr,store,pageth);
+// 		write_bs((i+FRAME0)*NBPG,store,pageth);
+// 	}
+//
+// }
+
+
+
+
+index = SETZERO;
+	while (index < TWOTEN) {
+		/* code */
+		int checkPIDNew = frm_tab[index].fr_pid;
+		int checkTypeNew = frm_tab[index].fr_type;
+
+		if (checkPIDNew == currpid && checkTypeNew == SETZERO) {
+			/* code */
+			int vpnoTolookup = frm_tab[index].fr_vpno;
+			// vpnoTolookup = vpnoTolookup * TWOTEN * 4;
+			hasToLookup = bsm_lookup(currpid, (vpnoTolookup*TWOTEN * 4), &store, &pageth);
+			if (hasToLookup == SYSERR) {
+				continue;
+			}
+			int frameInt = (index + TWOTEN)* TWOTEN * 4;
+			// frameInt = frameInt * TWOTEN * 4;
+			read_bs(frameInt, store, pageth);
+		}
+		index = index + SETONE;
+	}
+
+	// for(i=0;i<NFRAMES;++i){
+	// 	 if(frm_tab[i].fr_pid==currpid && frm_tab[i].fr_type==FR_PAGE){
+	// 		res_lookup=bsm_lookup(currpid,frm_tab[i].fr_vpno*NBPG,&store,&pageth);
+	// 		if(res_lookup==SYSERR){
+	// 			continue;
+	// 		}
+	// 		read_bs((i+FRAME0)*NBPG,store,pageth);
+	// 	}
+	//
+	// }
 
 
 
