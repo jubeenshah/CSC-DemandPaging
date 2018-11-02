43a70048bbfe618c776da236d37cfa2fee1d730f
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 18:41:36 -0400

    Update csc501-lab2/sys/resched.c

--- csc501-lab2/sys/resched.c
+++ csc501-lab2/sys/resched.c
@@ -86,9 +86,9 @@ int	resched()
 	PrintSaved(nptr);
 #endif
 
-	int a = *optr;
+	int a = &optr;
 	// int b = proctab;
-	int oldProcessID = a - proctab;
+	int oldProcessID = a - *proctab;
 	int old_pid = oldProcessID;
 	// int store;
 	// int pageth;
