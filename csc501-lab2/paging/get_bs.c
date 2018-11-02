35c15a96d4b02ee0d7e521e8f22ef27c9facadf9
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 09:29:48 -0400

    Update csc501-lab2/paging/get_bs.c

--- csc501-lab2/paging/get_bs.c
+++ csc501-lab2/paging/get_bs.c
@@ -5,18 +5,18 @@
 
 #define SETONE  1
 #define SETZERO 0
-#define NEIGHT  16
-#define NPAGES  128
+#define NEIGHT  16 //8
+#define NPAGES  128 //256
 int get_bs(bsd_t bs_id, unsigned int npages) {
 
 STATWORD ps;
 disable(ps);
   /* requests a new mapping of npages with ID map_id */
     kprintf("get_bs\n");
-    int checkSemValue = bsm_tab[bs_id].bs_sem;
+    int checkSemValue     = bsm_tab[bs_id].bs_sem;
     int checkPrivateValue = bsm_tab[bs_id].bs_private;
-    int checkStatus = bsm_tab[bs_id].bs_status;
-    int pagesValue = bsm_tab[bs_id].bs_npages;
+    int checkStatus       = bsm_tab[bs_id].bs_status;
+    int pagesValue        = bsm_tab[bs_id].bs_npages;
     if (bs_id >= NEIGHT || bs_id < SETZERO || npages > NPAGES ||
           npages <= SETZERO || checkSemValue == SETONE) {
       /* code */
