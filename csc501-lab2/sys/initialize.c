72bbba6dabbaf33cb38ae43fb657995994ec3ff8
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-02 20:41:18 -0400

    Update csc501-lab2/sys/initialize.c

--- csc501-lab2/sys/initialize.c
+++ csc501-lab2/sys/initialize.c
@@ -80,36 +80,52 @@ int sc_ptr;
  */
 
 void init_paging() {
-  SYSCALL pfintr();
-	int i,j;
-	/* modified */
-	init_bsm();  /* init bsm */
-	init_frm(); /* init frm */
+   SYSCALL pfintr();
+ int index       = SETZERO;
+ int indexDos    = SETZERO;
+ int i,j;
 
-	int frm_num=0;
-	pt_t *pt;
-	pd_t *pd;
-	for(i=0;i<4;++i){
-		get_frm(&frm_num);
-		frm_tab[frm_num].fr_type=FR_TBL;
-		frm_tab[frm_num].fr_status=FRM_MAPPED;
-		frm_tab[frm_num].fr_pid=NULLPROC;
-		pt=(FRAME0 + frm_num)*NBPG;
-		for(j=0;j<1024;++j){
-			pt->pt_pres=1;
-			pt->pt_write=1;
-			pt->pt_user=0;
-			pt->pt_pwt=0;
-			pt->pt_pcd=0;
-			pt->pt_acc=0;
-			pt->pt_dirty=0;
-			pt->pt_mbz=0;
-			pt->pt_global=1;
-			pt->pt_avail=0;
-			pt->pt_base=i*FRAME0 + j;
-			pt++;
-		}
-	}
+//kprintf("INIT DEMANDS PAGINF");
+ init_bsm();
+ init_frm();
+ int frameNumber = SETZERO;
+ int frm_num = SETZERO;
+
+ pt_t *pt;
+ pd_t *pd;
+ int limitA =  4;
+ while (index < limitA) {
+   /* code */
+   get_frm(&frameNumber);
+   frm_tab[frameNumber].fr_status  = SETONE;
+   frm_tab[frameNumber].fr_type    = SETONE;
+   frm_tab[frameNumber].fr_pid     = NULLPROC;
+
+   int a = (TWOTEN + frameNumber) * (TWOTEN * 4);
+   // a = a * TWOTEN * 4;
+   pt = a;
+   while (indexDos < TWOTEN) {
+     /* code */
+     pt->pt_pres   = SETONE;
+     pt->pt_write  = SETONE;
+     pt->pt_user   = SETZERO;
+     pt->pt_pwt    = SETZERO;
+     pt->pt_pcd    = SETZERO;
+     pt->pt_acc    = SETZERO;
+     pt->pt_mbz    = SETZERO;
+     pt->pt_dirty  = SETZERO;
+     pt->pt_global = SETONE;
+     pt->pt_avail  = SETZERO;
+     pt->pt_base = (index * TWOTEN) + indexDos;
+     pt++;
+     indexDos = indexDos + SETONE;
+   }
+   index = index + SETONE;
+ }
+ create_page_dir(NULLPROC);
+ set_pdbr(NULLPROC);
+ set_evec(14, pfintr);
+ enable_paging();
 	create_page_dir(NULLPROC);
 	set_pdbr(NULLPROC);/*Set the PDBR register to the page directory for the NULL process.*/
 	set_evec(14,pfintr);		/*Install the page fault interrupt service routine.*/
