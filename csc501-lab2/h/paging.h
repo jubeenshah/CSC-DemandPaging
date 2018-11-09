03fe988432e53078a618813af6a8581fa13cdf71
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-09 06:39:34 -0500

    Update csc501-lab2/h/paging.h

--- csc501-lab2/h/paging.h
+++ csc501-lab2/h/paging.h
@@ -195,8 +195,8 @@ SYSCALL write_bs(char *, bsd_t, int);
 #define FRAME0		1024	/* zero-th frame		*/
 #define NFRAMES 	1024	/* number of frames		*/
 /*modified*/
-#define NBS 8 /* number of backing store */
-#define NPPBS 256 /* number of pages per backing store */
+//#define NBS 8 /* number of backing store */
+//#define NPPBS 256 /* number of pages per backing store */
 
 
 
