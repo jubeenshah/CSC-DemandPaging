e0a96bfa7ba084ac92d091ccf81119fcde6dba0c
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-09 06:47:12 -0500

    Update csc501-lab2/paging/get_bs.c

--- csc501-lab2/paging/get_bs.c
+++ csc501-lab2/paging/get_bs.c
@@ -17,20 +17,20 @@ disable(ps);
     int checkPrivateValue = bsm_tab[bs_id].bs_private;
     int checkStatus       = bsm_tab[bs_id].bs_status;
     int pagesValue        = bsm_tab[bs_id].bs_npages;
-    if (bs_id >= NEIGHT || bs_id < SETZERO || npages > NPAGES ||
+    if (bs_id >= 8 || bs_id < SETZERO || npages > 256 ||
           npages <= SETZERO || checkSemValue == SETONE) {
       /* code */
       restore(ps);
       return SYSERR;
     }
 
-    if (checkPrivateValue == SETONE && checkStatus == SETONE) {
+    if (checkPrivateValue == SETONE && checkStatus == 1) {
       /* code */
       restore(ps);
       return SYSERR;
     }
 
-    if (checkPrivateValue == SETZERO && checkStatus == SETONE) {
+    if (checkPrivateValue == SETZERO && checkStatus == 1) {
       /* code */
       restore(ps);
       pagesValue = bsm_tab[bs_id].bs_npages;
@@ -47,35 +47,3 @@ disable(ps);
     return npages;
 
 }
-
-// #include <conf.h>
-// #include <kernel.h>
-// #include <proc.h>
-// #include <paging.h>
-//
-// int get_bs(bsd_t bs_id, unsigned int npages) {
-//
-//   /* requests a new mapping of npages with ID map_id */
-//   //  kprintf("To be implemented!\n");
-//   STATWORD ps;
-//   disable(ps);
-//
-//   if(bs_id>=NBS || bs_id<0 || npages>NPPBS || npages<=0 || bsm_tab[bs_id].bs_sem==1){
-//   	restore(ps);
-// 	return SYSERR;
-//   }
-//   if(bsm_tab[bs_id].bs_private==1 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
-//   	restore(ps);
-// 	return SYSERR;
-//   }
-//   if(bsm_tab[bs_id].bs_private==0 && bsm_tab[bs_id].bs_status== BSM_MAPPED){
-//   	restore(ps);
-// 	return bsm_tab[bs_id].bs_npages;
-//   }
-//   /* bsm_tab[bs_id].bs_status == BSM_UNMAPPED */
-// 	bsm_tab[bs_id].bs_npages=npages;
-// 	bsm_tab[bs_id].bs_pid[currpid]=1;
-//
-// 	restore(ps);
-//     return npages;
-// }
