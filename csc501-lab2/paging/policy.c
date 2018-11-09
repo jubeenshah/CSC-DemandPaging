b41930a471112e3c360c06ba05625fe2b5165967
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-09 06:23:52 -0500

    update

--- csc501-lab2/paging/policy.c
+++ csc501-lab2/paging/policy.c
@@ -28,32 +28,3 @@ SYSCALL grpolicy()
 {
   return page_replace_policy;
 }
-// /* policy.c = srpolicy*/
-//
-// #include <conf.h>
-// #include <kernel.h>
-// #include <paging.h>
-//
-//
-// extern int page_replace_policy;
-// /*-------------------------------------------------------------------------
-//  * srpolicy - set page replace policy
-//  *-------------------------------------------------------------------------
-//  */
-// SYSCALL srpolicy(int policy)
-// {
-//   /* sanity check ! */
-//
-//   //kprintf("To be implemented!\n");
-// 	page_replace_policy=policy;
-//   return OK;
-// }
-//
-// /*-------------------------------------------------------------------------
-//  * grpolicy - get page replace policy
-//  *-------------------------------------------------------------------------
-//  */
-// SYSCALL grpolicy()
-// {
-//   return page_replace_policy;
-// }
--- csc501-lab2/paging/release_bs.c
+++ csc501-lab2/paging/release_bs.c
@@ -31,32 +31,3 @@ SYSCALL release_bs(bsd_t bs_id) {
    return OK;
 
 }
-
-// #include <conf.h>
-// #include <kernel.h>
-// #include <proc.h>
-// #include <paging.h>
-//
-// SYSCALL release_bs(bsd_t bs_id) {
-//
-//   /* release the backing store with ID bs_id */
-//   //  kprintf("To be implemented!\n");
-//   STATWORD ps;
-//   disable(ps);
-//   bsm_tab[bs_id].bs_pid[currpid]=0;
-//   bsm_tab[bs_id].bs_vpno[currpid]=4096;
-//
-// if(bsm_tab[bs_id].bs_mapn==0){
-//   bsm_tab[bs_id].bs_status=BSM_UNMAPPED;
-//   bsm_tab[bs_id].bs_sem=0;
-//   //bsm_tab[bs_id].bs_pid=-1;
-//   bsm_tab[bs_id].bs_npages=0;
-//   //bsm_tab[bs_id].bs_vpno=4096;
-//   bsm_tab[bs_id].bs_private=0;
-//
-//   //kprintf("FINALLY release store%d\n",bs_id);
-// }
-// //	kprintf("release_bs pid:%d mapn:%d\n",currpid,bsm_tab[bs_id].bs_mapn);
-// 	restore(ps);
-// 	return OK;
-// }
