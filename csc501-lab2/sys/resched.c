dc492307684403a421deb6cbdb69cfd178a926c7
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-11 13:15:37 -0500

    Update csc501-lab2/sys/resched.c

--- csc501-lab2/sys/resched.c
+++ csc501-lab2/sys/resched.c
@@ -28,7 +28,7 @@ int	resched()
 	int pageth;
 	int hasToLookup;
 	int old_pid;
-	int oldProcessID;
+	int old_pid;
 	int index;
 
 	disable(PS);
@@ -93,20 +93,20 @@ int	resched()
 #endif
 
 
-	oldProcessID = optr - proctab;
+	old_pid = optr - proctab;
 	index = SETZERO;
-	old_pid = oldProcessID;
+	old_pid = old_pid;
 
 	while (index < TWOTEN) {
 		/* code */
 		int checkPid = frm_tab[index].fr_pid;
 		int checkTyp = frm_tab[index].fr_type;
-		if (checkPid == oldProcessID && checkTyp == SETZERO) {
+		if (checkPid == old_pid && checkTyp == SETZERO) {
 			/* code */
-			// int q = oldProcessID;
+			// int q = old_pid;
 			// int w = frm_tab[index].fr_vpno * TWOTEN * 4;
 
-			hasToLookup = bsm_lookup(oldProcessID,(frm_tab[index].fr_vpno * TWOTEN * 4),&store, &pageth);
+			hasToLookup = bsm_lookup(old_pid,(frm_tab[index].fr_vpno * TWOTEN * 4),&store, &pageth);
 			if (hasToLookup == SYSERR) {
 				continue;
 			}
